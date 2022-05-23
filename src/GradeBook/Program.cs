aa564b4e96d6d7779e34ee6855e7e0497ee5f413
Faysal Regal <faysalregal@gmail.com>
2022-05-23 19:36:17 +0100

    Initial commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, Scott!");
+
+using System;
+
+namespace GradeBook // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new[] {1.1, 2.2, 3.3};
+            var Grades = new List<double>() {12.7, 10.3, 6.11, 4.1};
+
+
+
+            var sum = 0.0;
+
+            foreach(var num in Grades) {
+                sum += num;
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(sum/(Grades.Count));
+        }
+    }
+}
