a4562f83ca9846b43e827e7423ff18462f845c1b
Faysal Regal <faysalregal@gmail.com>
2022-06-20 19:29:59 +0100

    added first test

--- test/GradeBook.tests/UnitTest1.cs
+++ test/GradeBook.tests/UnitTest1.cs
@@ -7,6 +7,17 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        //arrange
+        var x = 5;
+        var y = 2;
+        var expected = 8;
+
+        //act
+        var actual = x + y;
+
+        //assert
+        Assert.Equal(actual, expected);
+        
 
     }
 }
